9abcf1bef1efa80771646a3aba4f3c954af8e43f
Miguel Campuzano <developermiguel95@gmail.com>
2019-12-14 12:11:29 -0500

    act

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,18 @@
 import service.FunctionTest;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         FunctionTest functionTest = () -> System.out.println("Hola Mundo");
 
         functionTest.saludar();
+
+        Main main = new Main();
+        main.MiMetodo(functionTest);
+
+    }
+
+    public void MiMetodo(FunctionTest parametro) {
+        parametro.saludar();
     }
 }
